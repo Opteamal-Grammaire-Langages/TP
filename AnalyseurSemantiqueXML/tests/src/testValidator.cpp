c3569265b9a7a16666612eb9a8af5c0f40df9252
Thomas Fattal <nypias@gmail.com>
2012-04-23 23:00:33 +0200

    Ajout du test 4

--- AnalyseurSemantiqueXML/tests/src/testValidator.cpp
+++ AnalyseurSemantiqueXML/tests/src/testValidator.cpp
@@ -11,6 +11,29 @@ Document * testValidator::creationDTD() {
 	Element * elB = new Element ("B", "CDATA");
 	Element * elD = new Element ("D", "");
 
+	DTDattribut * attA = new DTDattribut ("a", "CDATA", "");
+	list<DTDattribut *> listeAtt;
+	listeAtt.push_back(attA);
+	pair <string, list<DTDattribut *> > paireAttA ("A", listeAtt);
+
+	document->addElement(elA);
+	document->addElement(elB);
+	document->addElement(elC);
+	document->addElement(elD);
+
+	document->addAttributsElement(paireAttA);
+
+	return document;
+}
+
+/* Creation d'une DTD ne comprenant pas d'attribut */
+Document * testValidator::creationDTDPasAttribut() {
+	Document * document = new Document();
+	Element * elA = new Element ("A", "B,CDATA,C");
+	Element * elC = new Element ("C", "D");
+	Element * elB = new Element ("B", "CDATA");
+	Element * elD = new Element ("D", "");
+
 	document->addElement(elA);
 	document->addElement(elB);
 	document->addElement(elC);
@@ -99,5 +122,21 @@ bool testValidator::testValidator3() {
 
 	Document doc;
 	Validator val (&doc);
+	return val.validateXML(xmlBalise);
+}
+
+/**
+ * Le XML comprend un attribut sur la balise A.
+ * La DTD ne comprend pas d'attribut sur la balise A.
+ * Le test doit planter.
+ */
+bool testValidator::testValidator4() {
+
+	XMLBalise * xmlBalise = creationArbreXML();
+	cout << "=== Arbre XML ===" << endl;
+	cout << xmlBalise->toString() << endl;
+
+	Document * doc = creationDTDPasAttribut();
+	Validator val (doc);
 	return val.validateXML(xmlBalise);
 }
