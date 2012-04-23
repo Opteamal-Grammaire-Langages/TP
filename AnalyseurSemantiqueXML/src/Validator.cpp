c9c9e6df6396441f578895d9a824758be6b8cabc
Thomas Fattal <nypias@gmail.com>
2012-04-23 22:58:40 +0200

    Ajout de la verification des attributs du XML par rapport a la DTD

--- AnalyseurSemantiqueXML/src/Validator.cpp
+++ AnalyseurSemantiqueXML/src/Validator.cpp
@@ -28,10 +28,29 @@ bool Validator::validateXML(XMLBalise * xml){
 	list<XMLElement *>::iterator it;
 	list<XMLElement *> listeElements(xml->getElements());
 
+	// Recuperation de la liste des attributs de la balise XML en cours
+	map<string,string> listeAttributes(xml->getAttributes());
+
 	cout << "Nom Balise : " << xml->getName() << endl;
 
 	if (!(docDTD->getDescriptionElement(xml->getName()).empty())) {
+
+		// Validation de la balise XML avec la DTD
 		regOK = validate(xml->getChildrenTypes(), docDTD->getDescriptionElement(xml->getName()));
+
+		// Validation des attributs avec la DTD
+		if (regOK == true) {
+			if (!listeAttributes.empty()) {
+
+				for(map<string, string>::iterator it_attribute = listeAttributes.begin();
+							it_attribute != listeAttributes.end() && regOK == true;
+							it_attribute++) {
+
+					regOK = validateDTD(it_attribute->first, docDTD->getAttribut(xml->getName()));
+				}
+			}
+		}
+
 	} else {
 		if (xml->getChildrenTypes().empty()) {
 			return true;
@@ -48,6 +67,21 @@ bool Validator::validateXML(XMLBalise * xml){
 	}
 
 	return regOK;
+}
+
+/*
+ * On vérifie que l'attribut du XML (premier argument) est bien dans la DTD
+ */
+bool Validator::validateDTD (string attName, list<DTDattribut*> attributsDTD) {
+
+	bool trouve = false;
+	list<DTDattribut *>::iterator it;
+	for(it = attributsDTD.begin(); it != attributsDTD.end() && !trouve ; it++){
+		if ((*it)->getNomAtt() == attName) {
+			trouve = true;
+		}
+	}
+	return trouve;
 }
 
 bool Validator::validate(const string xmlTypes, string descriptionTypeBalise){
