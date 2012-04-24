7c29e8c44f6039bf91908a9a28a435d84300eca2
Raphael Catolino <raphael.catolino@gmail.com>
2012-04-24 10:43:59 +0200

    stuff
    Signed-off-by: Raphael Catolino <raphael.catolino@gmail.com>

--- mainProgram/src/main.cpp
+++ mainProgram/src/main.cpp
@@ -1,31 +1,44 @@
 #include <iostream>
-#include <stdio.h>
-#include <string.h>
-#include "../ModelXML/src/XMLBalise.h"
-#include "../analyseurSyntaxique/src/analyseur.h"
+#include <string>
 
 using namespace std;
-
+void displayHelp()
+{
+  cout << "Synopsis :" << endl;
+  cout << "\tanalyzer [-a] [-t type] FILENAME\n" << endl;
+  cout << "Commands :" << endl;;
+  cout << "\t-h / --help : display this help.\n" << endl;
+  cout << "\t-a / --analyse : If no type is specified, parse FILENAME as an xml file and check its syntax.\n\
+                         If a DOCTYPE declaration is found, the corresponding DTD is parsed as well\n\
+                         If -t type is specified, check FILENAME syntax against \"type\" syntax.\n\
+                         In this case, if the file is an xml file, don't check the corresponding DTD" << endl;
+}
 int main(int argc, char **argv)
 {
+  string  * args = new string[argc];
+  for(int i=0; i<argc; i++) {
+    args[i].append(argv[i]);
+  }
+  switch(argc){
+    case 1:
+        cout<<"At least 2 arguments expected"<<endl;
+        cout<<"See 'analyzer -h' for more information"<<endl;
+      break;
+    case 2:
+      if(args[1]=="-h" || args[1]=="--help")
+      {
+        displayHelp();
+      }
+      else
+      {
+        displayHelp();
+      }
+      break;
+    default :
+        displayHelp();
+      break;
 
-	switch(argc){
-		case 1:
-				cout<<"At least 2 arguments expected"<<endl;
-				cout<<"See 'analizer -h' for more information"<<endl;
-			break;
-		case 2:
-			if(strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)
-		  {
-
-		  }
-		  else
-		  {
-
-		  }
-			break;
-
-	}
-
+  }
+  delete[] args;
   return 1;
 }
