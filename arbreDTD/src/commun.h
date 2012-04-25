edd40d76497ea0c37783205a4f3265692e369c40
Raphael Catolino <raphael.catolino@gmail.com>
2012-04-25 20:52:20 +0000

    rajoute des fichiers manquants dans arbreDTD
    Signed-off-by: Live session user <ubuntu@ubuntu.(none)>

---
+++ arbreDTD/src/commun.h
@@ -0,0 +1,31 @@
+#ifndef COMMUN_H
+#define COMMUN_H
+
+using namespace std;
+#include <list>
+#include <utility>
+#include <string>
+// Includes des fichiers pour la structure DTD
+#include "../../ModelDTD/src/Document.h"
+#include "../../ModelDTD/src/Element.h"
+#include "../../ModelDTD/src/DTDattribut.h"
+typedef pair<string,string> Attribut;
+typedef list<Attribut> AttList;
+typedef pair<string,string> ElementName;
+int analyseDtd(char * input);
+int analyseXml(int argc, char **argv);
+
+// Includes des fichiers pour la construction de la structure XML
+//#include "../../ModelXML/src/XMLElement.h"
+//#include "../../ModelXML/src/XMLBalise.h"
+//#include "../../ModelXML/src/XMLData.h"
+//On ne devrait pas en avoir besoin ici, seulement dans la partie arbreXML
+
+#ifdef DEBUG
+#define debug(x) (x)
+#else
+#define debug(x) ((void) 0)
+#endif
+
+
+#endif
