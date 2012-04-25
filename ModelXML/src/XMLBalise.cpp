0cc0bc2618640fc626637f10c41d27899e78da62
Thomas Fattal <nypias@gmail.com>
2012-04-25 20:40:35 +0200

    Ajout d'une methode addElements

--- ModelXML/src/XMLBalise.cpp
+++ ModelXML/src/XMLBalise.cpp
@@ -31,6 +31,15 @@ XMLBalise::~XMLBalise() {
 void XMLBalise::addElement(XMLElement * elem){
 	elements.push_back(elem);
 }
+
+void XMLBalise::addElements(list<XMLElement *> elem){
+
+	list<XMLElement *>::iterator it;
+	for(it = elem.begin(); it != elem.end() ; it++){
+		elements.push_back(*it);
+	}
+}
+
 void XMLBalise::setElementList(list<XMLElement*> * list){
   elements=*list;
   delete list;
@@ -159,9 +168,14 @@ string XMLBalise::getNameSpace(){
 	return ns;
 }
 
+/**
+ * Vérifie l'existence d'un noeud XLST qui matche avec le nom de la balise XML
+ * Si la balise est trouvee, renvoie la balise XLST
+ * Si la balise n'est pas trouvee, renvoie 0
+ */
 XMLBalise * XMLBalise::match( XMLBalise * xsl){
 
-	//Parcours de tous les elements XML
+	// Parcours de tous les elements XML du fichier XSL
 	for(list<XMLElement *>::iterator it_element = xsl->getElements().begin();
 			it_element != xsl->getElements().end() && xsl->getElements().size() > 0;
 			it_element++) {
