27d5ac57101d81fae6694452c3a11a1e6e9a9e72
michael fagno <michael.fagno@free.fr>
2012-04-27 09:24:54 +0200

    Match de la racine ajoute

--- ModelXML/src/XMLBalise.cpp
+++ ModelXML/src/XMLBalise.cpp
@@ -206,9 +206,18 @@ XMLBalise * XMLBalise::match( XMLBalise * xsl){
 		 	map<string,string> mapAttributs = balise->getAttributes();
 		 	string matchingName = mapAttributs.find("match")->second;
 
-		 	if (name.compare(matchingName) == 0 ){ //Si la valeur de l'attribut match est celle recherchee
-		 		return balise;
-		 	}
+			//Si on est pas la racine
+			if(this->parent == 0){
+				if (name.compare(matchingName) == 0 ){ //Si la valeur de l'attribut match est celle recherchee
+		 			return balise;
+		 		}
+			//Si on se trouve a la racine
+			}else{
+				if (matchingName.compare("/") == 0 ){ //Si la valeur de l'attribut match est celle recherchee
+		 			return balise;
+		 		}
+			}
+		 	
 		 }
 	}
 	return 0;
