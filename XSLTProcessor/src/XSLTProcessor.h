91497f4f85a0a0b4237b8e444cdcdcfd855f7291
mickah <michael.fagno@free.fr>
2012-04-29 19:04:58 +0300

    Methodes commentes

--- XSLTProcessor/src/XSLTProcessor.h
+++ XSLTProcessor/src/XSLTProcessor.h
@@ -1,8 +1,6 @@
 /*
  * XLSTProcessor.h
  *
- *  Created on: 25 avr. 2012
- *      Author: tommi
  */
 
 #ifndef XLSTPROCESSOR_H_
@@ -21,9 +19,37 @@ class XSLTProcessor {
 
 public:
 
+	/*
+	Methode qui transforme un document XML a partir d'un document xsl.
+	Parametres:
+		-docXML: document XML que l'on souhaite transformer
+		-xsl: document XSL devant etre applique sur le document xml
+		-racine: precise si le pointeur docXML pointe sur la racine du document XML ou non (pour le match "/").
+	Retour:
+		Une liste contenant le document genere. (la plupart du temps un seul element sera dans cette liste: le noeud racine)
+	*/
 	static list<XMLElement *> generateXSLXML(XMLBalise * docXML, XMLBalise * xls, bool racine = false);
 protected:
+	/*
+	Methode qui genere le contenu d'un template.
+	Parametres:
+		-templateMatched: template a generer
+		-xsl: document XSL devant etre applique sur le document xml
+		-noeudXMLMatched: noeud du document xml qui a matche.
+	Retour:
+		Le template genere.
+	*/
 	static list<XMLElement *> generateTemplate(XMLBalise * templateMatched, XMLBalise * xls, XMLBalise * noeudXMLMatched);
+	
+	/*
+	Methode qui genere un element d'un template.
+	Parametres:
+		-elementXSL: noeud XSL a generer
+		-xsl: document XSL devant etre applique sur le document xml
+		-xmlMatching: noeud du document xml qui a matche.
+	Retour:
+		Le template genere.
+	*/
 	static list<XMLElement *> lookOverXSLToBuildTemplate(XMLBalise * elementXSL, XMLBalise * xls, XMLBalise * xmlMatching);
 
 };
